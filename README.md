fb66a01cb3e3268603dad24c9eef47cc79c2721f
sssunho <sssunho.lee@gmail.com>
2018-09-23 06:59:10 +0900

    smart contract, 프로젝트 기획에 맞게 수정

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 EOS Hackathon London, eoswag
 
 
+## EOSIO
+
+Q. 블록체인에 무엇을 담을 것인가?
+
+누가, 언제, 어느 브랜드에서 체크인을 하였다.
+
+
+
+- struct: actions
+- function: takeaction(액션 기록하기), getaction(액션 불러오기)
+
+
+
+
+
+
 ## API
 
 ### /post
---
+++ blockiosk/blcokiosk.cpp
@@ -0,0 +1,53 @@
+/**
+  * @author sunny lee
+  * @date 2018-09-23
+  */
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+#include <eosio.token/eosio.token.hpp>
+#include <string>
+using namespace eosio;
+
+class blockiosk : public contract {
+    private:
+        /// @abi table actions
+        struct actions {
+            account_name    owner;
+            std::string     act_type;
+            std::string     where;
+            std::string     memo;
+            time            create_time;
+
+            uint64_t primary_key()const { return owner; }
+        };
+
+
+    public:
+        blockiosk( account_name self ):contract(self){}
+
+        typedef eosio::multi_index<N(accounts), accounts> _accounts;
+        typedef eosio::multi_index<N(actions), actions> _actions;
+
+        void takeaction( account_name owner, std::string act_type, std::string where, std::string memo){
+            _actions action(_self, _self);
+            action.emplace( _self, [&]( auto& a ){
+                a.action_type = action_type;
+                a.owner = owner;
+                a.act_type = act_type;
+                a.where = where;
+                a.memo = memo;
+                a.create_time = now();
+            });
+        }
+
+        void getaction( account_name owner ) {
+            _actions action(_self, _self);
+
+            auto act = action.get( owner );
+            auto act_obj = name{act.owner};
+            std::string act_str = act_obj.to_string();
+            print(act_str);
+        }
+};
+
+EOSIO_ABI( blockiosk, (takeaction)(getaction) )
