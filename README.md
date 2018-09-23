eaf7aa73cdd62bae0f0fa1a1bf212f964988991a
sssunho <sssunho.lee@gmail.com>
2018-09-23 10:05:40 +0900

    API 작성 완료
    가라로 넣었습니다. 일단 결과물이 제대로 나오는 것을 목표로..

--- README.md
+++ README.md
@@ -20,9 +20,38 @@ Q. 블록체인에 무엇을 담을 것인가?
 
 ## API
 
-### /post
+### /order
 
-데이터를 쌓는 API
+주문 결과를 저장하는 API
+
+#### request
+
+POST
+
+- order_id: 매장에서 제공하는 주문 번호. (옵션, 없어도 됨)
+- orderlist: 배열. 아래 예시 참고. 필수!
+```
+orderlist = [
+    {
+        'product_id': 1,
+        'quantity': 1,
+        'memo': '소금빼고주세요'
+    },
+    {
+        'product_id': 2,
+        'quantity': 2,
+        'memo': None
+    }
+]
+```
+
+#### response
+
+```
+{
+    "id": 4
+}
+```
 
 
 
@@ -31,3 +60,77 @@ Q. 블록체인에 무엇을 담을 것인가?
 
 사용자가 매장에서 체크인했을 때 데이터를 출력해주는 API
 
+#### request
+
+POST
+
+- user: 사용자 이름, 이메일.. 응 안써~ (옵션)
+- store: 브랜드 이름 (starbucks, mcdonalds, domino)
+- spot: 지점명 (옵션)
+- memo: (옵션)
+
+
+#### response
+
+- last: 최근 주문내역
+- brand_frequency: 브랜드별 빈도순 상위 세가지 메뉴
+- category_frequency: 브랜드가 해당하는 카테고리별 빈도순 상위 세가지 메뉴
+
+```
+{
+    "brand_frequency": [
+        {
+            "memo": null,
+            "product_id": 2,
+            "product_name": "Coca-Cola(Large)",
+            "quantity": 1
+        },
+        {
+            "memo": null,
+            "product_id": 11,
+            "product_name": "Big Mac",
+            "quantity": 1
+        },
+        {
+            "memo": null,
+            "product_id": 12,
+            "product_name": "4 piece Chicken McNuggets",
+            "quantity": 1
+        }
+    ],
+    "category_frequency": [
+        {
+            "memo": null,
+            "product_id": 1,
+            "product_name": "Chips",
+            "quantity": 1
+        },
+        {
+            "memo": null,
+            "product_id": 2,
+            "product_name": "Coca-Cola(Large)",
+            "quantity": 1
+        },
+        {
+            "memo": null,
+            "product_id": 3,
+            "product_name": "Vanilla Cone",
+            "quantity": 1
+        }
+    ],
+    "last": [
+        {
+            "memo": null,
+            "product_id": 11,
+            "product_name": "Big Mac",
+            "quantity": 1
+        },
+        {
+            "memo": null,
+            "product_id": 2,
+            "product_name": "Coca-Cola(Large)",
+            "quantity": 1
+        }
+    ]
+}
+```
--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import pymysql
 import json
 from io import BytesIO
 import urllib
+from eospy import EosClient
+from eospy.transaction_builder import TransactionBuilder, Action
 
 app = Flask(__name__)
 
@@ -13,7 +15,7 @@ app = Flask(__name__)
 with open('config.json', 'r') as f:
     conf = json.load(f)
 DATABASE = conf['mysql']
-HYCON = conf['hycon']
+EOS = conf['eosio']
 
 
 def connect_db():
@@ -28,24 +30,302 @@ def connect_db():
 def hello_world():
     return 'Hello World!'
 
-@app.route('/checkin')
+@app.route('/checkin', methods=['POST'])
 def checkin():
-    return
+    conn = connect_db()
+    cur = conn.cursor()
 
-@app.route('/post')
-def post(self, endpoint, uri, body=None):
-    if body:
-        body = json.dumps(body).encode()
-    url = urllib.parse.urljoin(endpoint, uri)
-    request = urllib.request.Request(url, data=body)
-    # response = urllib.request.urlopen(request)
-    # return json.load(response)
-    x = urllib.request.urlopen(request)
-    raw_data = x.read()
-    encoding = x.info().get_content_charset('utf8')  # JSON default
-    data = json.loads(raw_data.decode(encoding))
-    return data
+    user = request.values.get('user', None)
+    store = request.values.get('store', None)
+    spot = request.values.get('spot', None)
+    memo = request.values.get('memo', None)
 
+    # 블록체인에 누가, 언제, 어떤 브랜드에 checkin 했는지 기록하기
+    args = {
+        "action": "takeaction",
+        "code": "blockiosk",
+        "args": {
+            "owner": "user",
+            "act_type": "checkin",
+            "where": store,
+            "memo": memo
+        }
+    }
+    try:
+        send_action('takeaction', args)
+    except:
+        pass
+
+    # Off DB에도 저장하기
+    try:
+        query = """INSERT INTO actions (username, store, spot, memo) VALUES(%s, %s, %s, %s);"""
+        cur.execute(query, args=(user, store, spot, memo, ))
+
+        # Off DB에서 사용자가 지난번에 시킨 내역 + 그 브랜드 카테고리의 빈도수 메뉴대로 출력
+        # query = """SELECT """
+        # cur.execute(query, ('', ))
+        # ret = cur.fetchall()
+
+        if store == 'starbucks':
+            data = {
+                "last": [
+                    {
+                        'product_id': 21,
+                        'product_name': 'Ice Cold Brew',
+                        'quantity': 1,
+                        'memo': 'Give me coffee in the mug.'
+                    },
+                    {
+                        'product_id': 22,
+                        'product_name': 'Butter Croissant',
+                        'quantity': 1,
+                        'memo': 'Please warm.'
+                    }
+                ],
+                "brand_frequency": [
+                    {
+                        'product_id': 21,
+                        'product_name': 'Ice Cold Brew',
+                        'quantity': 1,
+                        'memo': 'Give me coffee in the mug.'
+                    },
+                    {
+                        'product_id': 23,
+                        'product_name': 'Caffè Latte',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 24,
+                        'product_name': 'Shortbread Biscuit',
+                        'quantity': 1,
+                        'memo': None
+                    }
+                ],
+                "category_frequency": [
+                    {
+                        'product_id': 23,
+                        'product_name': 'Caffè Latte',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 25,
+                        'product_name': 'Flat White',
+                        'quantity': 1,
+                        'memo': 'Please put a spoonful of sugar.'
+                    },
+                    {
+                        'product_id': 26,
+                        'product_name': 'Chocolate Brownie',
+                        'quantity': 1,
+                        'memo': 'Please warm.'
+                    }
+                ]
+            }
+        elif store == 'mcdonals':
+            data = {
+                "last": [
+                    {
+                        'product_id': 11,
+                        'product_name': 'Big Mac',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 2,
+                        'product_name': 'Coca-Cola(Large)',
+                        'quantity': 1,
+                        'memo': None
+                    }
+                ],
+                "brand_frequency": [
+                    {
+                        'product_id': 2,
+                        'product_name': 'Coca-Cola(Large)',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 11,
+                        'product_name': 'Big Mac',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 12,
+                        'product_name': '4 piece Chicken McNuggets',
+                        'quantity': 1,
+                        'memo': None
+                    }
+                ],
+                "category_frequency": [
+                    {
+                        'product_id': 1,
+                        'product_name': 'Chips',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 2,
+                        'product_name': 'Coca-Cola(Large)',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 3,
+                        'product_name': 'Vanilla Cone',
+                        'quantity': 1,
+                        'memo': None
+                    }
+                ]
+            }
+        else:
+            data = {
+                "last": [
+                    {
+                        'product_id': 31,
+                        'product_name': 'Original Cheese & Tomato',
+                        'quantity': 2,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 2,
+                        'product_name': 'Coca-Cola(Large)',
+                        'quantity': 2,
+                        'memo': None
+                    }
+                ],
+                "brand_frequency": [
+                    {
+                        'product_id': 32,
+                        'product_name': 'Chicken Feast',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 33,
+                        'product_name': 'Ham & Pineapple',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 2,
+                        'product_name': 'Coca-Cola(Large)',
+                        'quantity': 2,
+                        'memo': None
+                    }
+                ],
+                "category_frequency": [
+                    {
+                        'product_id': 2,
+                        'product_name': 'Coca-Cola(Large)',
+                        'quantity': 2,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 33,
+                        'product_name': 'Ham & Pineapple',
+                        'quantity': 1,
+                        'memo': None
+                    },
+                    {
+                        'product_id': 34,
+                        'product_name': 'Ranch BBQ',
+                        'quantity': 1,
+                        'memo': 'Please warm.'
+                    }
+                ]
+            }
+
+        cur.close()
+        conn.commit()
+        conn.close()
+        return make_response(jsonify(**data), 200)
+    except:
+        traceback.print_exc()
+        # conn.rollback()
+        error = {
+            "code": 2222,
+            "type": "ServerError",
+            "message": "Server is temporary unavailable."
+        }
+        return make_response(jsonify(error=error), 500)
+
+@app.route('/order', methods=['POST'])
+def order():
+    # 주문번호 + 배열로 제품번호, 수량 받기
+    order_id = request.values.get('order_id', None)
+    orderlist = request.values.get('order', None)
+    # orderlist = [
+    #     {
+    #         'product_id': 1,
+    #         'quantity': 1,
+    #         'memo': '소금빼고주세요'
+    #     },
+    #     {
+    #         'product_id': 2,
+    #         'quantity': 2,
+    #         'memo': None
+    #     }
+    # ]
+
+    conn = connect_db()
+    cur = conn.cursor()
+
+    # Off DB에 주문내역 저장
+    try:
+        query = """INSERT INTO orders (number) VALUES(%s);"""
+        cur.execute(query, args=(order_id, ))
+        new_id = cur.lastrowid
+
+        for p in orderlist:
+            query = """INSERT INTO orders_detail (order_id, product_id, quantity, memo) VALUES(%s, %s, %s, %s);"""
+            cur.execute(query, args=(new_id, p['product_id'], p['quantity'], p['memo'], ))
+
+        cur.close()
+        conn.commit()
+        conn.close()
+        return make_response(jsonify(id=new_id), 200)
+    except:
+        traceback.print_exc()
+        # conn.rollback()
+        error = {
+            "code": 2222,
+            "type": "ServerError",
+            "message": "Server is temporary unavailable."
+        }
+        return make_response(jsonify(error=error), 500)
+
+def send_action(action_name, args):
+    eos_conn = EosClient(api_endpoint="http://fabius.ciceron.xyz:8888", wallet_endpoint="http://fabius.ciceron.xyz:8888")
+    txBuilder = TransactionBuilder(eos_conn)
+
+    try:
+        eos_conn.wallet_unlock(EOS['wallet'])
+    except:
+        pass
+
+    # param = {
+    #     "action": "writeaction",
+    #     "code": "david",
+    #     "args": {
+    #         "user": "david",
+    #         "action_type": action_name,
+    #         "objective": "",
+    #         "amount": point_amount
+    #     }
+    # }
+    try:
+        bin_param = eos_conn.chain_abi_json_to_bin(args)
+        act = Action("blockiosk", action_name, "blockiosk", "active", bin_param['binargs'])
+        ready_tx, chain_id =  txBuilder.build_sign_transaction_request([act])
+        signed_transaction = eos_conn.wallet_sign_transaction(ready_tx, ['EOS7vtZvYDke1PVemFjAbnoEBmQCSkUtYg2CznRajsUdEP2r7pk7r'], chain_id)
+        ret = eos_conn.chain_push_transaction(signed_transaction)
+        return ret
+    except:
+        traceback.print_exc()
+        return
 
 if __name__ == '__main__':
     # app.run()
@@ -54,5 +334,5 @@ if __name__ == '__main__':
 
     cert = './cert_key/pfx.mycattool_com.crt'
     key = './cert_key/pfx.mycattool_com.key'
-    https = WSGIServer(('0.0.0.0', 5000), app, keyfile=key, certfile=cert)
+    https = WSGIServer(('0.0.0.0', 5001), app, keyfile=key, certfile=cert)
     https.serve_forever()
